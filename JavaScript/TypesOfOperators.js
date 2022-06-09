a69cfd5dbfa3968b52babd1189afb6ccd0f3caa6
Vinay Kuresi <hello@vinaykumarkuresi.com>
2022-06-09 18:09:37 +0530

    May Full Stack Batch

---
+++ JavaScript/TypesOfOperators.js
@@ -0,0 +1,12 @@
+1. Arithematic
+2. Assignment
+3. Relational
+4. Logical
+5. Conditional
+/*
+Arithematic Operator : +, -, *, /, %, ++, --
+Assignment Operator : =, +=, -=, *=, /=, %=
+Relational Operator : >, <, >=, <=, ==, !=
+*/
+
+
